34ccd5110aef65d592fbf394d5e3858ea9381875
Aaron Young <ahyoung@bsu.edu>
2017-02-14 11:55:16 -0500

    Created draw, rules, and tutorial pages Linked buttons on home screen with pages respectively Locked orientation for all pages in manifest

--- app/src/main/java/chemia/httpsgithub/comahyoung1/chemia/MainActivity.java
+++ app/src/main/java/chemia/httpsgithub/comahyoung1/chemia/MainActivity.java
@@ -1,7 +1,10 @@
 package chemia.httpsgithub.comahyoung1.chemia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button tutorialBtn = (Button) findViewById(R.id.tutorial_btn);
+        tutorialBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent tutorialPage = new Intent(getApplicationContext(), chemia.httpsgithub.comahyoung1.chemia.tutorialPage.class);
+                startActivity(tutorialPage);
+            }
+        });
+
+        Button rulesBtn = (Button) findViewById(R.id.rules_btn);
+        rulesBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent rulesPage = new Intent(getApplicationContext(), chemia.httpsgithub.comahyoung1.chemia.rulesPage.class);
+                startActivity(rulesPage);
+            }
+        });
+        Button drawBtn = (Button) findViewById(R.id.draw_btn);
+        drawBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent drawPage = new Intent(getApplicationContext(), chemia.httpsgithub.comahyoung1.chemia.drawPage.class);
+                startActivity(drawPage);
+            }
+        });
+
     }
 }
